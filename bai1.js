b8c1ea838c27effeeacb2a5f602cf9621a589e84
ponchibi <78527717+ponchibi@users.noreply.github.com>
2021-08-21 20:28:21 +0700

    Create bai1.js

---
+++ bai1.js
@@ -0,0 +1,6 @@
+var ListStudent=[
+    {name:'Lê Thành Nhân',ID:51800033,LỚP:18020304,Gender:"Male"},
+    {name:'Lê Thị Giàu Sang',ID:5180055,LỚP:18020304,Gender:"Female"},
+    {name:'Phạm Phú Quý',ID:51800011,LỚP:18020304,Gender:"Male"}
+    ];
+console.log(ListStudent);
