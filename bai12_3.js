fe5c8cee165c226dd4d2a1df53de078ac44a1c53
ponchibi <78527717+ponchibi@users.noreply.github.com>
2021-08-23 21:26:36 +0700

    Create bai12_3.js

---
+++ bai12_3.js
@@ -0,0 +1,14 @@
+var a=10;
+function so(a)
+{
+    if (a % 2 ==0)
+    {
+      console.log("a là số chẵn");
+    }
+    else (a % 2 !=0)
+    {
+      console.log ("a là số lẽ");
+    }
+    
+}
+so(3); 
